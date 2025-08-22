5e7b88cd5c6e2616eec8db9569590647c9eaa9cd
UmmIt <git@ummit.dev>
2025-08-23 00:05:56 +0800

    style: add arch icon into homepage title

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
+import { Icon } from '@iconify/react';
 
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">UmmIt Docs</h1>
+      <h1 className="mb-4 text-2xl font-bold"><Icon icon="mdi:arch" className="inline-block mr-2" width={30} height={30} /> UmmIt Docs</h1>
       <h3 className="mb-4 text-gray-400">Here you can find the documentation about all the UmmIt Side project usage like UmmItOS.</h3>
       <p className="text-fd-muted-foreground">
         You can open{' '}
