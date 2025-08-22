3a9b2d8c0147a8e588693e0ae968a0e677655a7e
UmmIt <git@ummit.dev>
2025-08-22 23:42:08 +0800

    docs: update documentation titles and descriptions for me (UmmIt Kin)

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,7 +3,8 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+      <h1 className="mb-4 text-2xl font-bold">UmmIt Docs</h1>
+      <h3 className="mb-4 text-gray-400">Here you can find the documentation about all the UmmIt Side project usage like UmmItOS.</h3>
       <p className="text-fd-muted-foreground">
         You can open{' '}
         <Link
@@ -12,7 +13,7 @@ export default function HomePage() {
         >
           /docs
         </Link>{' '}
-        and see the documentation.
+        to start reading my documentation :)
       </p>
     </main>
   );
